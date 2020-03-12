08ad0cc9dd6d95fddc1256d6e880c1987166ec20
mici <raymone.mc@alibaba-inc.com>
2020-03-12 09:03:03 +0800

    project init

---
+++ basic-train/basic-1.py
@@ -0,0 +1,12 @@
+'''
+@Description: code
+@Author: MiCi
+@Date: 2020-03-12 08:55:59
+@LastEditTime: 2020-03-12 09:02:45
+@LastEditors: MiCi
+'''
+
+import pandas as pd
+
+if __name__ == '__main__':
+    print('Start about Learn pandas)
