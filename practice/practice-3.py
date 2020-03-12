2ff5187bbbea47d108be7a81ebe68ee2ecb34806
MiCigo <micihaha@aliyun.com>
2020-03-12 22:39:49 +0800

    complete practice-3

--- practice/practice-3.py
+++ practice/practice-3.py
@@ -2,7 +2,7 @@
 @Description: code
 @Author: MiCi
 @Date: 2020-03-12 21:11:37
-@LastEditTime: 2020-03-12 21:47:58
+@LastEditTime: 2020-03-12 22:39:23
 @LastEditors: MiCi
 '''
 
@@ -16,7 +16,27 @@ class Practice3(object):
 
     def exercise(self):
         path = './data/drinks.csv'
-        dinksData = pd.read_csv(path)
+        drinksData = pd.read_csv(path)
+        print(drinksData.head(10))
+
+        # 哪个continent平均消耗beer最高
+        print('Avg Beer:\n', drinksData.groupby(
+            'continent')['beer_servings'].mean())
+
+        # 打印continent的wine_servings描述性统计值
+        # describe 包括：计数、平均值、标准差、最小值、最大值等
+        print(drinksData.groupby('continent').wine_servings.describe())
+
+        # 打印出每个continent每种酒类的消耗平均值
+        print(drinksData.groupby('continent').mean())
+
+        # 打印出每个continent每种酒类的中位数
+        print(drinksData.groupby('continent').median())
+
+        # 打印出每个continent队spirit的平均值、最大值、最小值
+        print(drinksData.groupby('continent')[
+              'spirit_servings'].agg(['mean', 'max', 'min']))
+
         return
 
 
