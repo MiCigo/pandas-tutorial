a1484aca1b0a24c4232dec06bec14973a760edff
mici <raymone.mc@alibaba-inc.com>
2020-03-12 09:05:48 +0800

    update .gitignore

--- basic-train/basic-1.py
+++ basic-train/basic-1.py
@@ -2,11 +2,11 @@
 @Description: code
 @Author: MiCi
 @Date: 2020-03-12 08:55:59
-@LastEditTime: 2020-03-12 09:02:45
+@LastEditTime: 2020-03-12 09:05:34
 @LastEditors: MiCi
 '''
 
 import pandas as pd
 
 if __name__ == '__main__':
-    print('Start about Learn pandas)
+    print('Start about Learn pandas')
