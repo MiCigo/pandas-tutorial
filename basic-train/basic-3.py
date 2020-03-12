2b8288ebb8a5d2468caeee5666887e922ed9df10
MiCigo <micihaha@aliyun.com>
2020-03-12 21:50:32 +0800

    Update basic 1~3

--- basic-train/basic-3.py
+++ basic-train/basic-3.py
@@ -2,7 +2,7 @@
 @Description: code
 @Author: MiCi
 @Date: 2020-03-12 21:11:37
-@LastEditTime: 2020-03-12 21:43:21
+@LastEditTime: 2020-03-12 21:49:23
 @LastEditors: MiCi
 '''
 
@@ -11,13 +11,12 @@ import pandas as pd
 
 class Basic3(object):
 
-   def __init__(self):
+    def __init__(self):
         return
 
     def basic_use(self):
-        path = './data/drinks.csv'
-        dinksData = pd.read_csv(path)
         return
+
 
 if __name__ == '__main__':
     print('Start Learn Pandas')
